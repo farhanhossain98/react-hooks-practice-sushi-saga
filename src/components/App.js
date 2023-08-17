eb64f4a3cfa371c81235b024c6fe29418fd91c44
farhan hossain <farhanrufon99@gmail.com>
2023-08-16 23:45:24 -0400

    finished product

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [index, setIndex] = useState(0)
+  const [eatenSushis, setEatenSushis] = useState([])
+  const [wallet, setWallet] = useState(100)
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((sushi) => setSushis(sushi))
+  }, [])
+
+  const fourSushi = sushis.slice(index, index + 4)
+
+  function moreSushi() {
+    setIndex(index + 4)
+    if (index >= 96) {
+      return setIndex(0)
+    }
+  }
+
+  function eatSushi(sushi) {
+    if (!eatenSushis.includes(sushi.id) && wallet >= sushi.price) {
+      return (
+        setWallet(wallet - sushi.price),
+        setEatenSushis([...eatenSushis, sushi.id])
+      )
+    }
+  }
+
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        sushis={fourSushi}
+        moreSushi={moreSushi}
+        eatSushi={eatSushi}
+        eatenSushis={eatenSushis}
+      />
+      <Table
+        wallet={wallet}
+        plates={eatenSushis}
+
+      />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, moreSushi, eatSushi, eatenSushis }) {
+  const renderSushis = sushis.map((sushi) => {
+    return <Sushi
+      key={sushi.id}
+      sushi={sushi}
+      eatSushi={eatSushi}
+      eatenSushis={eatenSushis}
+    />
+  })
+
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushis}
+      <MoreButton
+        moreSushi={moreSushi}
+
+      />
     </div>
   );
 }
